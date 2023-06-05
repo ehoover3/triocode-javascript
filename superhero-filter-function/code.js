01f2edb19abf097d0e78722d59efe0c3072e77af
ehoover3 <ehoover3@yahoo.com>
2023-06-05 10:22:27 -0400

    first commit

---
+++ superhero-filter-function/code.js
@@ -0,0 +1,214 @@
+////////////////////////////
+// DATA ////////////////////
+////////////////////////////
+
+// This 'heroes' data is to set up the assignment
+// you will start writing your code at line 149 of this file
+
+let heroes = [
+    {
+      name: "Batman",
+      gender: "male",
+      mainColor: "black",
+      powers: { superspeed: false, canFly: false, usesMagic: false },
+      home: "Gotham",
+      height: { meters: 1.88, inches: 74 },
+    },
+    {
+      name: "Superman",
+      gender: "male",
+      mainColor: "blue",
+      powers: { superspeed: true, canFly: true, usesMagic: false },
+      home: "Fortress of Solitude",
+      height: { meters: 1.91, inches: 75 },
+    },
+    {
+      name: "Flash",
+      gender: "male",
+      mainColor: "red",
+      powers: { superspeed: false, canFly: false, usesMagic: false },
+      home: "Central City",
+      height: { meters: 1.83, inches: 72 },
+    },
+    {
+      name: "Aquaman",
+      gender: "male",
+      mainColor: "orange",
+      powers: { superspeed: false, canFly: false, usesMagic: false },
+      home: "Atlantis",
+      height: { meters: 1.93, inches: 76 },
+    },
+    {
+      name: "Green_Arrow",
+      gender: "male",
+      mainColor: "green",
+      powers: { superspeed: false, canFly: false, usesMagic: false },
+      home: "Star City",
+      height: { meters: 1.85, inches: 73 },
+    },
+    {
+      name: "Hawkman",
+      gender: "male",
+      mainColor: "green",
+      powers: { superspeed: false, canFly: true, usesMagic: false },
+      home: "Ancient Egypt",
+      height: { meters: 1.85, inches: 73 },
+    },
+    {
+      name: "Robin",
+      gender: "male",
+      mainColor: "red",
+      powers: { superspeed: false, canFly: false, usesMagic: false },
+      home: "Gotham",
+      height: { meters: 1.63, inches: 64 },
+    },
+    {
+      name: "Supergirl",
+      gender: "female",
+      mainColor: "blue",
+      powers: { superspeed: false, canFly: true, usesMagic: false },
+      home: "Midvale Orphanage",
+      height: { meters: 1.47, inches: 58 },
+    },
+    {
+      name: "Green_Lantern",
+      gender: "male",
+      mainColor: "green",
+      powers: { superspeed: false, canFly: true, usesMagic: false },
+      home: "Oa",
+      height: { meters: 1.88, inches: 74 },
+    },
+    {
+      name: "Cyborg",
+      gender: "male",
+      mainColor: "grey",
+      powers: { superspeed: false, canFly: false, usesMagic: false },
+      home: "Detroit",
+      height: { meters: 1.73, inches: 68 },
+    },
+    {
+      name: "Shazaam",
+      gender: "male",
+      mainColor: "red",
+      powers: { superspeed: false, canFly: true, usesMagic: true },
+      home: "Fewcett City",
+      height: { meters: 1.93, inches: 76 },
+    },
+    {
+      name: "Batgirl",
+      gender: "female",
+      mainColor: "black",
+      powers: { superspeed: false, canFly: false, usesMagic: false },
+      home: "Gotham",
+      height: { meters: 1.93, inches: 76 },
+    },
+    {
+      name: "Firestorm",
+      gender: "male",
+      mainColor: "red",
+      powers: { superspeed: false, canFly: true, usesMagic: false },
+      home: "New York City",
+      height: { meters: 1.88, inches: 74 },
+    },
+    {
+      name: "Zatanna",
+      gender: "female",
+      mainColor: "black",
+      powers: { superspeed: false, canFly: false, usesMagic: true },
+      home: "New York City",
+      height: { meters: 1.7, inches: 67 },
+    },
+    {
+      name: "Constantine",
+      gender: "male",
+      mainColor: "grey",
+      powers: { superspeed: false, canFly: false, usesMagic: true },
+      home: "Northumberland",
+      height: { meters: 1.8, inches: 71 },
+    },
+  ];
+  
+  //////////////////////////////////////////////////////
+  // ACCESSING DATA IN AN ARRAY OF OBJECTS PRACTICE ////
+  //////////////////////////////////////////////////////
+  addSectionHeader("ACCESSING DATA IN AN ARRAY OF OBJECTS PRACTICE");
+  //
+  // 1. Use the 'heroes' variable to put Batman's name in the below function
+  // The answer will render the word 'Batman'
+  addInstructions("1. Use the 'heroes' variable to put Batman's name in the below function");
+  addData(heroes[0].name);
+  
+  // 2. Use the 'heroes' variable to put Aquaman's main color in the below function
+  // The answer will render the word 'orange'
+  addInstructions("2. Use the 'heroes' variable to put Aquaman's main color in the below function");
+  addData(heroes[3].mainColor);
+  
+  // 3. Use the 'heroes' variable to put Aquaman's home in the below function
+  // The answer will render the word 'Atlantis'
+  addInstructions("3. Use the 'heroes' variable to put Aquaman's home in the below function");
+  addData();   // add your code inside the addData function call
+  
+  // 4. Use the 'heroes' variable to put if superman can fly in the below function
+  // The answer will render the word 'true'
+  addInstructions("4. Use the 'heroes' variable to put if superman can fly in the below function");
+  addData();   // add your code inside the addData function call
+  
+  // 5. Use the 'heroes' variable to put if superman uses magic in the below function
+  // The answer will render the number '1.91'
+  addInstructions("5. Use the 'heroes' variable to put superman's height in meters");
+  addData();   // add your code inside the addData function call
+  
+  ////////////////////////////
+  // FILTER METHOD PRACTICE //
+  ////////////////////////////
+  addSectionHeader("FILTER METHOD PRACTICE");
+  //
+  // 1. Show all heroes
+  // The answer will render Batman, Superman, Flash, Aquaman, Green_Arrow, Hawkman, Robin, Supergirl, Green_Lantern, Cyborg, Shazaam, Batgirl, Firestorm, Zatanna, Constantine
+  addInstructions("1. Show all heroes");
+  addHero(heroes);
+  
+  // 2. Show only Batman
+  // The answer will render Batman
+  addInstructions("2. Show only Batman");
+  addHero(heroes.filter((hero)=>hero.name === 'Batman'));
+  
+  // 3. Show heroes who are female
+  // The answer will render Supergirl, Batgirl, Zatanna
+  addInstructions("3. Show heroes who are female");
+  addHero(heroes.filter(hero => hero.gender === 'female'));  // <-- uncomment this function & add your code inside the function call
+  
+  // 4. Show heroes whose main color is black
+  // The answer will render Batman, Batgirl, Zatanna
+  addInstructions("4. Show heroes whose main color is black");
+  // addHero();  // <-- add your code inside the addHero function call
+  
+  // 5. Show heroes who can fly
+  // The answer will render Superman, Hawkman, Supergirl, Green_Lantern, Shazaam, Firestorm
+  addInstructions("5. Show heroes who can fly");
+  // addHero();  // <-- add your code inside the addHero function call
+  
+  // 6. Show heroes who are female and main color is black
+  // The answer will render Batgirl, Zatanna
+  addInstructions("6. Show heroes who are female and main color is black");
+  // addHero();  // <-- add your code inside the addHero function call
+  
+  // 7. Show heroes who can fly and main color is NOT red
+  // The answer will render Superman, Hawkman, Supergirl, Green_Lantern
+  addInstructions("7. Show heroes who can fly and main color is NOT red");
+  // addHero();  // <-- add your code inside the addHero function call
+  
+  // 8. Show heroes shorter than 1.80 meters
+  // The answer will render Robin, Supergirl, Cyborg, Zatanna
+  addInstructions("8. Show heroes shorter than 1.80 meters");
+  // addHero();  // <-- add your code inside the addHero function call
+  
+  // 9. Show heroes taller than 1.90 meters
+  // The answer will render Superman, Aquaman, Shazaam, Batgirl
+  addInstructions("9. Show heroes taller than 1.90 meters");
+  // addHero();  // <-- add your code inside the addHero function call
+  
+  // 10. Show heroes taller than 1.85 meters & shorter than 1.9 meters
+  // The answer will render Batman, Green_Lantern, Firestorm
+  addInstructions("10. Show heroes taller than 1.85 meters & shorter than 1.9 meters");
+  // addHero();  // <-- add your code inside the addHero function call
---
+++ superhero-filter-function/functions.js
@@ -0,0 +1,38 @@
+// This file is to set up the assignment
+// You will write your code inside code.js
+
+// SELECTORS
+const root = document.querySelector("#root");
+
+// FUNCTIONS
+function addSectionHeader(sectionName) {
+  let h1 = document.createElement("h1");
+  h1.textContent = sectionName;
+  root.append(h1);
+}
+
+function addInstructions(instruction, answer = "") {
+  let div = document.createElement("div");
+  div.innerHTML = `<h2>${instruction}</h2><div>${answer}</div>`;
+  root.append(div);
+}
+
+function addData(data) {
+  let div = document.createElement("div");
+  div.textContent = data;
+  root.append(div);
+}
+
+function addHero(array) {
+  let container = document.createElement("div");
+  container.className = "container";
+  for (let i = 0; i < array.length; i++) {
+    let hero = document.createElement("div");
+    hero.className = 'heroImg'
+    hero.innerHTML = `
+          <img id='${array[i].name}'/>
+          <div id='heroName'>${array[i].name}</div>`;
+    container.append(hero);
+  }
+  root.append(container);
+}
